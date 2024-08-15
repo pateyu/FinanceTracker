051d99e67e6539658f9c796938e7c8a0d03e0a9e
pateyu <yugbpatel30@gmail.com>
2024-08-15 17:29:46 -0500

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Yug Patel on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
